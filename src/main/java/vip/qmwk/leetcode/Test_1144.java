38e442e047472d4438a637041bf57a777648be66
zhenkun.hu 胡震坤 <zhenkun.hu@qiyuesuo.com>
2020-08-20 20:48:21 +0800

    Test_1144

---
+++ src/main/java/vip/qmwk/leetcode/Test_1144.java
@@ -0,0 +1,36 @@
+package vip.qmwk.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: HZK
+ * @Date: 2020/08/20/20:09
+ * @Description:
+ */
+public class Test_1144 {
+
+    public static int movesToMakeZigzag(int[] nums) {
+        int[] copyNums = Arrays.copyOf(nums, nums.length);
+        // 奇数位比临值小
+        int count1 = count(nums, 1);
+        int count2 = count(copyNums, 0);
+        return Math.min(count1, count2);
+    }
+
+    private static int count(int[] nums, int beginIndex){
+        int count = 0;
+        for (int i = beginIndex; i < nums.length; i = i+2) {
+            if(i-1>=0 && nums[i-1]<=nums[i]){
+                count += nums[i] - nums[i-1] + 1;
+                nums[i] = nums[i-1] - 1;
+            }
+            if(i+1<nums.length && nums[i+1]<=nums[i]){
+                count += nums[i] - nums[i+1] + 1;
+                nums[i] = nums[i+1] - 1;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/main/java/vip/qmwk/leetcode/test/Test.java
@@ -0,0 +1,18 @@
+package vip.qmwk.leetcode.test;
+
+import vip.qmwk.leetcode.Test_1144;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: HZK
+ * @Date: 2020/08/20/20:11
+ * @Description:
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int[] nums = {9,6,1,6,2};
+        System.out.println(Test_1144.movesToMakeZigzag(nums));
+    }
+}
